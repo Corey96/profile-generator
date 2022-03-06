27b94f1361d7cebe59160482f530b39149ef5bde
Corey96 <0coreyy@gmail.com>
2022-03-06 20:36:23 +0000

    working script template

---
+++ generate.js
@@ -0,0 +1,12 @@
+// inuirer all the stuff
+// read the tempalte
+//fill out the template with the stuff
+//write the templated file to index.html
+
+const inquirer = require("inquirer");
+
+async function generate() {
+  console.log("hello");
+}
+
+generate();
